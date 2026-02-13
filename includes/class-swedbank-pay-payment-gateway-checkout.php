b494a3bc7fb0e2b39b8a96f6e0178247f3ff70e5
anyakro <anya@krokedil.se>
2026-02-13 11:16:05 +0100

    Change indentation for method chain

--- includes/class-swedbank-pay-payment-gateway-checkout.php
+++ includes/class-swedbank-pay-payment-gateway-checkout.php
@@ -451,10 +451,10 @@ class Swedbank_Pay_Payment_Gateway_Checkout extends WC_Payment_Gateway {
 			$args['icon']            = plugins_url( 'assets/images/checkout.svg', __DIR__ );
 			$gateway_page            = new Gateway( $this, $args );
 			$args['general_content'] = array( $gateway_page, 'output' );
-			( SettingsPage::get_instance() )
-			->set_plugin_name( 'Swedbank Pay Payment Menu for WooCommerce' )
-			->register_page( $this->id, $args, $this )
-			->output( $this->id );
+			SettingsPage::get_instance()
+				->set_plugin_name( 'Swedbank Pay Payment Menu for WooCommerce' )
+				->register_page( $this->id, $args, $this )
+				->output( $this->id );
 		}
 
 		$this->display_errors();
