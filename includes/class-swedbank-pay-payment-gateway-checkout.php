dc21eaf8b65137d46145598a039e76bdf1bece1f
krokedilmartin <84283336+krokedilmartin@users.noreply.github.com>
2026-04-09 09:07:22 +0200

    remove setting Language. map to get_locale() instead

--- includes/class-swedbank-pay-payment-gateway-checkout.php
+++ includes/class-swedbank-pay-payment-gateway-checkout.php
@@ -162,7 +162,7 @@ class Swedbank_Pay_Payment_Gateway_Checkout extends WC_Payment_Gateway {
 		$this->access_token                 = $this->settings['access_token'] ?? $this->access_token;
 		$this->payee_id                     = $this->settings['payee_id'] ?? $this->payee_id;
 		$this->testmode                     = $this->settings['testmode'] ?? $this->testmode;
-		$this->culture                      = $this->settings['culture'] ?? $this->culture;
+		$this->culture                      = self::locale_to_culture( get_locale() );
 		$this->logo_url                     = $this->settings['logo_url'] ?? $this->logo_url;
 		$this->instant_capture              = $this->settings['instant_capture'] ?? $this->instant_capture;
 		$this->terms_url                    = $this->settings['terms_url'] ?? get_site_url();
@@ -201,6 +201,37 @@ class Swedbank_Pay_Payment_Gateway_Checkout extends WC_Payment_Gateway {
 			->set_mode( wc_string_to_bool( $this->testmode ) ? Swedbank_Pay_Api::MODE_TEST : Swedbank_Pay_Api::MODE_LIVE );
 
 		$this->payment_actions_handler = new Swedbank_Pay_Payment_Actions( $this );
+	}
+
+	/**
+	 * Map a WordPress locale string to a Swedbank Pay culture code.
+	 *
+	 * @param string $locale WordPress locale, e.g. 'sv_SE'.
+	 * @return string Swedbank Pay culture code, e.g. 'sv-SE'. Falls back to 'en-US'.
+	 */
+	public static function locale_to_culture( string $locale ): string {
+		$map = array(
+			'da_DK' => 'da-DK',
+			'de_DE' => 'de-DE',
+			'de_AT' => 'de-DE',
+			'de_CH' => 'de-DE',
+			'es_ES' => 'es-ES',
+			'et'    => 'et-EE',
+			'et_EE' => 'et-EE',
+			'fi'    => 'fi-FI',
+			'fi_FI' => 'fi-FI',
+			'fr_FR' => 'fr-FR',
+			'fr_BE' => 'fr-FR',
+			'lt_LT' => 'lt-LT',
+			'lv'    => 'lv-LV',
+			'lv_LV' => 'lv-LV',
+			'nb_NO' => 'nb-NO',
+			'pl_PL' => 'pl-PL',
+			'ru_RU' => 'ru-RU',
+			'sv_SE' => 'sv-SE',
+		);
+
+		return $map[ $locale ] ?? 'en-US';
 	}
 
 	/**
@@ -285,25 +316,6 @@ class Swedbank_Pay_Payment_Gateway_Checkout extends WC_Payment_Gateway {
 
 					return $value;
 				},
-			),
-			'culture'                     => array(
-				'title'       => __( 'Language', 'swedbank-pay-payment-menu' ),
-				'type'        => 'select',
-				'options'     => array(
-					'da-DK' => __( 'Danish', 'swedbank-pay-payment-menu' ),
-					'en-US' => __( 'English', 'swedbank-pay-payment-menu' ),
-					'et-EE' => __( 'Estonian', 'swedbank-pay-payment-menu' ),
-					'fi-FI' => __( 'Finnish', 'swedbank-pay-payment-menu' ),
-					'lt-LT' => __( 'Lithuanian', 'swedbank-pay-payment-menu' ),
-					'lv-LV' => __( 'Latvian', 'swedbank-pay-payment-menu' ),
-					'nb-NO' => __( 'Norwegian', 'swedbank-pay-payment-menu' ),
-					'sv-SE' => __( 'Swedish', 'swedbank-pay-payment-menu' ),
-				),
-				'description' => __(
-					'Language of pages displayed by Swedbank Pay during payment.',
-					'swedbank-pay-payment-menu'
-				),
-				'default'     => $this->culture,
 			),
 			'instant_capture'             => array(
 				'title'          => __( 'Instant Capture', 'swedbank-pay-payment-menu' ),
